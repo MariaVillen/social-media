38f53971a20cd3c2287399a20e61572ddf2c3cbf
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-05-09 23:53:57 +0200

    login and signup ui

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,38 @@
+import classes from './Login.module.scss';
+import LoginForm from '../../components/login-form/LoginForm';
+import SignUpForm from '../../components/singup-form/SignUpForm';
+
+import {useState} from 'react';
+
+export default function Login() {
+
+    const [isLoginView, setIsLoginView] = useState(true);
+
+    const changeViewHandler = (newViewState)=>{
+        setIsLoginView(newViewState);
+    }
+
+
+  return (
+    <div className={classes.login}>
+        <div className={classes.wrapper}>
+            <div className={classes.login_title}>
+                <div className={classes.logo}>
+                    <img src="/images/logos/icon-left-fontre.png" alt="Groupomania, reséau social d'enterprise" />
+                </div>
+                <span className={classes.description}>
+                    Commencez à partager avec vos collègues!
+                </span>
+            </div>
+            <div className={classes.login_body}>
+                <div className={classes.login_body_form}>
+                    {isLoginView ? 
+                    <LoginForm onChangeFormView={changeViewHandler}/> :
+                    <SignUpForm onChangeFormView={changeViewHandler}/>
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
