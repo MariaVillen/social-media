45af280a8d273ffadf284ff740114bee04d46a3f
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2022-05-13 16:01:39 +0200

    Profile-page

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,19 @@
 import classes from './Admin.module.scss';
-import Addpost from '../../components/addpost/Addpost';
+import PostComment from '../../components/post-comment/PostComment.component';
+
 
 export default function Admin() {
   return (
     <div>
-      admin
+      <h1> Admin Page </h1>
+       <PostComment placeHolder="Je veux commenter!"/>
+       
+       <h2>Gerer les utilisateurs</h2>
+       <h3>Utilisateurs non actifs</h3>
+       <h3>Utilisateurs actifs</h3>
+       <h3>Utilisateurs effacées</h3>
+       <h2>Gerer les signalements</h2>
+       
     </div>
   )
 }
