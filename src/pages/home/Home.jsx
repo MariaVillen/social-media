2b6ea73166e09c73ab610770da7282083b3722b9
Martin Kun <tinbrc2010k@hotmail.com>
2022-05-14 00:19:41 -0300

    Adding new hook 'useMediaQuery' which allows the rendering of components based on whether the query matches or not.
    (taken from: https://usehooks-ts.com/react-hook/use-media-query)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,16 +3,23 @@ import Feed from "../../components/feed/Feed.component";
 import Sidebar from "../../components/sidebar/Sidebar.component";
 import Rightbar from "../../components/rightbar/Rightbar.component";
 import Share from "../../components/share/Share.component";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 export default function Home() {
+  const matches = useMediaQuery("(min-width: 768px)");
+
   return (
     <div className={classes.home_container}>
-      <Sidebar className={classes.home_sidebar} />
-      <div className={classes.home_feed_content} >
-        <Share/>
-        <Feed/>
+      {matches ? <Sidebar className={classes.home_sidebar} /> : <></>}
+      <div className={classes.home_feed_content}>
+        <Share />
+        <Feed />
       </div>
-      <Rightbar className={classes.home_more_comment_content} />
+      {matches ? (
+        <Rightbar className={classes.home_more_comment_content} />
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
